270ebd6ac20a7c0fba76dbe5cb234c4d8adb7b8a
Debasish Bose <debasish.bose@fairfaxmedia.com.au>
2014-03-28 19:13:31 +1100

    Missed a rule-set

--- fms_crawler.rb
+++ fms_crawler.rb
@@ -6,27 +6,27 @@ require 'builder'
 require './crawler'
 
 domains = {
-  # :wedding => {
-  #   :domain => "http://www.wedding.com.au",
-  #   :white_lists => [
-  #         /\/wedding-news\//i,
-  #         /\/special-offers\//i,
-  #         /\/testimonials\//i,
-  #         /\/list-your-business\//i,
-  #         /\/faq\//i,
-  #         /\/contact\//i,
-  #         /\/tips-advice\//i,
-  #         /\/photo-galleries\//i
-  #   ],
-  #   :black_lists => [
-  #         /\/#/,
-  #         /\/quote\//,
-  #         /\/directory\//,
-  #         /\/article\//,          
-  #         /secure\.omg\.com\.au/i,
-  #         /secure_omg/i
-  #   ]
-  # }
+  :wedding => {
+    :domain => "http://www.wedding.com.au",
+    :white_lists => [
+          /\/wedding-news\//i,
+          /\/special-offers\//i,
+          /\/testimonials\//i,
+          /\/list-your-business\//i,
+          /\/faq\//i,
+          /\/contact\//i,
+          /\/tips-advice\//i,
+          /\/photo-galleries\//i
+    ],
+    :black_lists => [
+          /\/#/,
+          /\/quote\//,
+          /\/directory\//,
+          /\/article\//,          
+          /secure\.omg\.com\.au/i,
+          /secure_omg/i
+    ]
+  },
 
   :lawyers => {
     :domain => "http://www.lawyers.com.au",
